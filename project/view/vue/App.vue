58fc20d74df7099ac297af4b7bf0960abbf1f570
Gogo <gogo81745.moe@gmail.com>
2019-04-29 22:42:20 +0800

    Finish MainPage fade in animation

--- project/view/vue/App.vue
+++ project/view/vue/App.vue
@@ -1,7 +1,16 @@
 <template>
-    <component
-            v-bind:is="currentPage" v-on:toggle-page="togglePage($event.page)"
-    ></component>
+    <div id="g-body">
+
+        <div id="g-top-container">
+            <component v-bind:is="topBar"></component>
+        </div>
+        <div id="g-main-container" class="g-page-container">
+            <component
+                    v-bind:is="currentPage" v-on:toggle-page="togglePage($event.page)"
+            ></component>
+        </div>
+
+    </div>
 </template>
 
 <script>
@@ -11,12 +20,14 @@
         name: "App",
         data() {
             return {
-                currentPage: 'mainPage'
+                currentPage: 'mainPage',
+                topBar: 'topJumbotron'
             }
         },
         computed: {},
         components: {
             'mainPage': MainPage,
+            'topJumbotron': null,
         },
         methods: {
             togglePage: function (page) {
--- project/view/vue/MainPage.vue
+++ project/view/vue/MainPage.vue
@@ -1,10 +1,56 @@
 <template>
-    <h1>Hello</h1>
+    <main id="m-main-page">
+        <div class="m-keyword-area">
+
+            <div class="m-keyword" v-for="k of keywords" v-bind:class="{ 'v-fade-in': k.fadeIn,'v-hide': k.hide }">
+                <div class="u-keyword">{{k.keyword}}</div>
+                <div class="u-detail">{{k.detail}}</div>
+            </div>
+
+        </div>
+    </main>
 </template>
 
 <script>
     export default {
-        name: "MainPage"
+        name: "MainPage",
+        data() {
+            return {
+                keywords: [],
+            }
+        },
+        mounted() {
+            this.loadKeywords()
+        },
+        methods: {
+            getKeywords: function () {
+                return [
+                    {keyword: "TEXT QWQ", detail: "25 条商品信息"},
+                    {keyword: "TEXT QWQ", detail: "25 条商品信息"},
+                    {keyword: "TEXT QWQ", detail: "25 条商品信息"},
+                    {keyword: "TEXT QWQ", detail: "25 条商品信息"},
+                    {keyword: "TEXT QWQ", detail: "25 条商品信息"},
+                    {keyword: "TEXT QWQ", detail: "25 条商品信息"}
+                ]
+            },
+            loadKeywords: function () {
+                return new Promise((resolve, reject) => {
+                    let ks = this.getKeywords();
+                    ks.forEach(it => {
+                        it.hide = true
+                    });
+                    this.keywords.push(...ks);
+
+                    for (let i = 0; i < ks.length; i++) {
+                        setTimeout(() => {
+                            ks[i].hide = false;
+                            ks[i].fideIn = true;
+                        }, i * 100);
+                    }
+                    setTimeout(resolve, ks.length * 100 + 300);
+                });
+            }
+        }
     }
 </script>
 
